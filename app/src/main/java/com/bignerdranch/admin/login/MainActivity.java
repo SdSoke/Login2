515f8acd649570d03cf8b8a6ffb0ce6bcf048e94
SdSoke <948128323@qq.com>
2016-11-25 20:41:39 +0800

    Initial commit

---
+++ app/src/main/java/com/bignerdranch/admin/login/MainActivity.java
@@ -0,0 +1,49 @@
+package com.bignerdranch.admin.login;
+
+import android.content.Context;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    private Button mTrueButton;
+    private Button mFalseButton;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mTrueButton = (Button) findViewById(R.id.next_button);
+        mTrueButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setClass(MainActivity.this, NextActivity.class);
+               MainActivity.this.startActivity(intent);
+            }
+        });
+
+        mTrueButton = (Button) findViewById(R.id.forget_button);
+        mTrueButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {Toast.makeText(MainActivity.this,
+                    R.string.incorrect_toast,
+                    Toast.LENGTH_SHORT).show();
+
+            }
+        });
+        mFalseButton = (Button) findViewById(R.id.loin_button);
+        mFalseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {   Toast.makeText(MainActivity.this,
+                    R.string.correct_toast,
+                    Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/bignerdranch/admin/login/NextActivity.java
@@ -0,0 +1,19 @@
+package com.bignerdranch.admin.login;
+
+import android.app.ActionBar;
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by admin on 2016/11/24.
+ */
+
+public class NextActivity extends AppCompatActivity {
+
+    protected void onCreate(Bundle savedInstanceState)
+{
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.two);
+
+}}
